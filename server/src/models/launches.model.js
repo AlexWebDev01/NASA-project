8db32b6463377192cb96cf0feb8f74413a2b2a7b
Alex Udodov <alex.udodov.web@gmail.com>
2023-12-19 16:34:10 +0200

    feat: finished api pagination

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,19 +6,6 @@ const planets = require('./planets.mongo');
 const DEFAULT_FLIGHT_NUMBER = 100;
 const SPACEX_API_URL = 'https://api.spacexdata.com/v5/launches/query';
 
-const launch = {
-    flightNumber: 100, // flight_number
-    mission: 'Kepler Exploration X', // name
-    rocket: 'Explorer IS1', // rocket.name
-    launchDate: new Date('December 27, 2030'), // date_local
-    target: 'Kepler-442 b', // not applicable
-    customer: ['ZTM', 'NASA'], // payload.customers for each payload
-    upcoming: true, // upcoming
-    success: true, // success
-};
-
-saveLaunch(launch);
-
 async function populateLaunches() {
     console.log('Downloading launch data...');
     const response = await axios.post(SPACEX_API_URL, {
@@ -99,6 +86,7 @@ async function existsLaunchWithId(launchId) {
 async function getAllLaunches (skip, limit) {
     return await launchesDatabase
         .find({}, { '_id': 0, '__v': 0 })
+        .sort({ flightNumber: 1 })
         .skip(skip)
         .limit(limit);
 }
